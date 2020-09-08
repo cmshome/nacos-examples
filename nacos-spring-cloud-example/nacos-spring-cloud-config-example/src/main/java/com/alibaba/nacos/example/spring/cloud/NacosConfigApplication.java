cae0288d90f34bbe96ea1ecb53656201a076cfce
李学凯 <cmshome@163.com>
2020-09-08 14:52:18 +0800

    其它模块的代码调整

--- nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConfigApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConfigApplication.java
@@ -2,6 +2,7 @@ package com.alibaba.nacos.example.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Document: https://nacos.io/zh-cn/docs/quick-start-spring-cloud.html
@@ -10,7 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NacosConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NacosConfigApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosConfigApplication.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :"+userName+"; age: "+userAge);
     }
 }
 
