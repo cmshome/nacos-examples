40d82322d697b9ec247614bd44e16594133fc739
李学凯 <cmshome@163.com>
2020-09-08 15:46:03 +0800

    Spring cloud nacos config 测试

--- nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConfigApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConfigApplication.java
@@ -6,15 +6,20 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Document: https://nacos.io/zh-cn/docs/quick-start-spring-cloud.html
+ *
+ * 在 Nacos Spring Cloud 中，dataId 的完整格式如下：
+ * ${prefix}-${spring.profiles.active}.${file-extension}
+ *
+ * prefix 默认为 spring.application.name 的值，也可以通过配置项 spring.cloud.nacos.config.prefix来配置。
+ * spring.profiles.active 即为当前环境对应的 profile，
+ * 注意：当 spring.profiles.active 为空时，对应的连接符 - 也将不存在，dataId 的拼接格式变成 ${prefix}.${file-extension}
+ * file-exetension 为配置内容的数据格式，可以通过配置项 spring.cloud.nacos.config.file-extension 来配置。目前只支持 properties 和 yaml 类型。
  */
 @SpringBootApplication
 public class NacosConfigApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosConfigApplication.class, args);
-        String userName = applicationContext.getEnvironment().getProperty("user.name");
-        String userAge = applicationContext.getEnvironment().getProperty("user.age");
-        System.err.println("user name :"+userName+"; age: "+userAge);
     }
 }
 
--- nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/controller/ConfigController.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/controller/ConfigController.java
@@ -1,9 +1,13 @@
 package com.alibaba.nacos.example.spring.cloud.controller;
 
+import com.alibaba.nacos.example.spring.cloud.util.JsonUtils;
+import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/config")
@@ -12,12 +16,21 @@ public class ConfigController {
 
     @Value("${useLocalCache:false}")
     private boolean useLocalCache;
+    @Value(value = "${name:lxk-test}")
+    private String name;
+
+    @Value(value = "${age:18}")
+    private int age;
 
     /**
      * http://localhost:8080/config/get
      */
     @RequestMapping("/get")
-    public boolean get() {
-        return useLocalCache;
+    public String get() {
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("useLocalCache", useLocalCache);
+        map.put("name", name);
+        map.put("age", age);
+        return JsonUtils.parseObjToFormatJson(map);
     }
 }
